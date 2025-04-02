03ee4161f1e23c305c6901db6757655ade5b8a40
budzow <michal.budzowski@sonarsource.com>
2025-04-02 12:44:51 +0200

    Create ex.cpp

---
+++ ex.cpp
@@ -0,0 +1,18 @@
+// With nodiscard
+[[nodiscard]] int someFunc1();
+
+
+void badUse1() {
+  someFunc1();  // Both S5277 and M23_007 are reported
+}
+
+// Without nodiscard
+int someFunc2();
+
+void badUse2() {
+  someFunc2(); // M23_007 is reported
+}
+
+int goodUse() {
+  return someFunc1();
+}
